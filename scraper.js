9e10ace699c60953b2782937c6abf9d057e47911
Noel <noel_emmanuel@pm.me>
2024-09-27 16:16:32 +1000

    Refactor scraper.js for error handling

--- scraper.js
+++ scraper.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const ProgressBar = require('progress');
 
-// function to read urls from a file
+// read urls from a file
 function readUrlsFromFile(filePath) {
     return fs.readFileSync(filePath, 'utf8').split('\n').filter(Boolean);
 }
@@ -11,8 +11,8 @@ async function scrapeTweet(url) {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle2' });
-    // Extract tweet data
-    const tweetData = await page.evaluate(() => {
+    // Extract x post's text data
+    const tweetData = await page.evaluate((url) => {
         const contentElement = document.querySelector('div[data-testid="tweetText"]');
         const tweetContent = contentElement ? contentElement.textContent : `Couldn't find tweet content at ${url}`;
         const userNameElement = document.querySelector('div[data-testid="User-Name"]');
@@ -21,19 +21,19 @@ async function scrapeTweet(url) {
             userName: userName.trim(),
             content: tweetContent.trim(),
         };
-    });
+    }, url);
     // Format content for Markdown with indentation
     const indentedContent = tweetData.content.split('\n').map(line => `\t${line}`).join('\n');
     const formattedContent = `\n${tweetData.userName}:\n\n${indentedContent}\n`;
     await browser.close();
-    // console.log(formattedContent);
     return formattedContent;
 }
 
 async function main() {
     const urlsFilePath = process.argv[2];
-    if (!urlsFilePath) {
-        console.error('Please provide a file path containing tweet URLs');
+    const outputFilePath = process.argv[3];
+    if (!urlsFilePath || !outputFilePath) {
+        console.error('Please provide a file with URLs to scrape and an output file path');
         process.exit(1);
     }
     const urls = readUrlsFromFile(urlsFilePath);
@@ -41,11 +41,15 @@ async function main() {
     for (const url of urls) {
         try {
             const content = await scrapeTweet(url);
-            fs.appendFileSync('output.md', `${content}\n\n`);
+            fs.appendFileSync(outputFilePath, `${content}\n\n`);
         } catch (error) {
             console.error(`Error scraping tweet at ${url}: ${error.message}`);
         }
         bar.tick();
     }
+    process.exit(0);
 }
-main().catch(console.error);
+main().catch(error => {
+    console.error(error);
+    process.exit(1);
+});
